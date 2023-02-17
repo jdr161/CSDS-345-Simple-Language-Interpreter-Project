7cd58309fcc0dc208a8a6953d6354c83708ccde6
James Redding <jdr161@case.edu>
2023-02-17 12:57:51 -0500

    Defined functions that need to be completed

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,1 +1,20 @@
 #lang racket
+(require "simpleParser.rkt")
+
+(define addBinding)   ; takes a name, value, and a state, and adds that key-value pair to the state and returns the state
+(define removeBinding ; takes a name and a state and removes that key-value pair from the state. Returns the state
+
+
+(define M_boolean) ; takes
+(define M_value)   ; takes
+
+(define M_declaration) ;
+(define M_assignment)  ;
+(define M_return)      ;
+(define M_if)          ;
+(define M_while)       ;
+  
+(define M_state)   ; takes 
+
+(define interpret
+  (lambda (filename)))
