76174263a6350624555e5123af76bd85834daeeb
James Redding <jdr161@case.edu>
2023-02-22 01:36:49 -0500

    bug fixes up to t13

--- interpreter.rkt
+++ interpreter.rkt
@@ -62,7 +62,9 @@
             expr
             (findBinding expr state))]
       [(eq? (car expr) '+) (+ (M_value (car (cdr expr)) state) (M_value (car (cdr (cdr expr))) state))]
-      [(eq? (car expr) '-) (- (M_value (car (cdr expr)) state) (M_value (car (cdr (cdr expr))) state))]
+      [(eq? (car expr) '-) (if (null? (cdr (cdr expr)))
+                               (- (M_value (car (cdr expr)) state))
+                               (- (M_value (car (cdr expr)) state) (M_value (car (cdr (cdr expr))) state)))] 
       [(eq? (car expr) '*) (* (M_value (car (cdr expr)) state) (M_value (car (cdr (cdr expr))) state))]
       [(eq? (car expr) '/) (/ (M_value (car (cdr expr)) state) (M_value (car (cdr (cdr expr))) state))]
       [(eq? (car expr) '%) (modulo (M_value (car (cdr expr)) state) (M_value (car (cdr (cdr expr))) state))]
@@ -96,7 +98,7 @@
     (cond
       [(declared? (car (cdr statement)) state) (error "variable name is already taken")]
       [(null? (cdr (cdr statement)))           (addBinding (car (cdr statement)) null state)]
-      (else                                    (addBinding (car (cdr statement)) (car (cdr (cdr statement))) state)))))
+      (else                                    (addBinding (car (cdr statement)) (M_value (car (cdr (cdr statement))) state) state)))))
 
 ; M_assignment takes an assignment statement (in the form (= variable expression)) and a state
 ; WW
@@ -117,7 +119,7 @@
 (define M_return
   (lambda (statement state)
     (cond
-      [(not (list? statement))       (addBinding 'return (M_value statement state) state)] ; statement is a number, variable, 'true, or 'false
+      [(not (list? (car (cdr statement))))       (addBinding 'return (M_value (car (cdr statement)) state) state)] ; statement is a number, variable, 'true, or 'false
       [(eq? (car statement) '==)     (addBinding 'return (eq? (M_value (car (cdr statement)) state) (M_value (car (cdr (cdr statement))) state)) state)] ; statement is a boolean expression
       [(eq? (car statement) '!=)     (addBinding 'return (not (eq? (M_value (car (cdr statement)) state) (M_value (car (cdr (cdr statement))) state)) state))]
       [(eq? (car statement) '<)      (addBinding 'return (< (M_value (car (cdr statement)) state) (M_value (car (cdr (cdr statement))) state)) state)]
@@ -138,7 +140,7 @@
     (cond
       [(null? statements) state] ; i assume if passed in it is not gonna be null though
       [(M_boolean (car (cdr statements)) state) (M_state (cons (car (cdr (cdr statements))) '()) state)]
-      [(eq? (car (cdr (cdr (cdr statements)))) '()) state] ;check if the else statemet is nullthen just return state as it is
+      [(eq? (cdr (cdr (cdr statements))) '()) state] ; check if the else statemet is null then just return state as it is
       [else (M_state (cons (car (cdr (cdr (cdr statements)))) '()) state)]))) ; return the false statement state
 
 
@@ -176,21 +178,24 @@
 ; returns the return value from that syntax tree
 (define interpret
   (lambda (filename)
-    (findBinding 'return (M_state(parser filename) '())))) ; () shows returns true for (null? '())
+    (findBinding 'return (M_state(parser filename) '((return ())))))) ; () shows returns true for (null? '())
 
-(parser "t20.txt")
-(interpret "t20.txt")
-;t2 fails and returns 6
-;t4 fails and returns (return (/ (- (* 5 7) 3) 2)) but it is 16!
-;t5 fails and returns '((x 10) (y (+ 12 x)) (return ((x 10) (y (+ 12 x)) (return 10))))
-;t6 fails and return x which is fine because x <= y and m=x so should return  5
-;t6 still ^((x 5) (y 6) (m x) (return x))
-;t7 fails and return y which is fine because x <= y and m=y so should return  6
-;t7 still ^((x 5) (y 6) (m y) (return y))
-;t9 need to fix M_if due to  car: contract violation expected: pair? given: '()
-;t10 fails '((return ((return ((return 6))))))
-;t12 fails because result didn't give error and return '((y x) (return x))
-;t13 fails because it didn't give error and return '((x (+ x y)) (y ()) (return (+ x y)))
+(parser "t13.txt")
+(interpret "t13.txt")
+;t1 runs and returns 150
+;t2 runs and returns -3 (6/11)
+;t3 runs and returns 10
+;t4 runs and returns 16
+;t5 runs and returns 220
+;t6 runs and returns 5
+;t7 runs and returns 6
+;t9 runs are returns 5
+;t10 runs and returns -39
+;t11 gives correct error
+;t12 gives correct error
+;t13 fails-> no check for null return from findBinding
+;t14 gives correct error
+
 ;t15 fails due to contract violation expected: pair? given: 'true
 ;t17 fails similar kind of error as t15
 ;t18 fails similar kind of error as t17
