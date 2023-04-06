feeb28bf5430473fc17f0013d62413c639b9f2a1
James Redding <jdr161@case.edu>
2023-04-05 20:50:55 -0400

    Added new parser

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,6 +1,6 @@
 ; If you are using scheme instead of racket, comment these two lines, uncomment the (load "simpleParser.scm") and comment the (require "simpleParser.rkt")
 #lang racket
-(require "simpleParser.rkt")
+(require "functionParser.rkt")
 ; (load "simpleParser.scm")
 
 
